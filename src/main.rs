e09182de8571ad056f6519aca9ec9307040569c5
Alex Muirhead <alexandermuirhead@hotmail.com>
2026-03-10 21:04:28 +1000

    Extract PGM into struct

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,65 @@
 use std::fs::File;
 use std::io::{Error, Write};
 
+#[derive(Debug)]
+struct PGM {
+    width: usize,
+    height: usize,
+    max_value: u8,
+    data: Vec<u8>,
+}
+
+impl PGM {
+    fn new(width: usize, height: usize, max_value: u8) -> Self {
+        let data = vec![0; width * height];
+        PGM {
+            width,
+            height,
+            max_value,
+            data,
+        }
+    }
+
+    fn write(&self, output: &mut File) -> Result<(), Error> {
+        // Header
+        write!(
+            output,
+            "P2\n{} {}\n{}\n",
+            self.width, self.height, self.max_value
+        )?;
+        // Data
+        for idx in 0..(self.width * self.height) {
+            write!(output, "{} ", self.data[idx])?;
+        }
+
+        Ok(())
+    }
+}
+
 fn main() -> Result<(), Error> {
     // --- No libraries at first! --
 
     // Let's write a simple PBM file
     let width = 9;
     let height = 16;
-    let max_value = 15;
+    let max_value = 255;
 
     let filename = "render.pgm";
     let mut output = File::create(filename)?;
 
-    // Header
-    writeln!(output, "P2")?;
-    writeln!(output, "{width} {height}")?;
-    writeln!(output, "{max_value}")?;
+    let mut image = PGM::new(width, height, max_value);
 
     // Some fake data to help me get started
-    let mut data = vec![0; width * height];
     for row in 0..height {
         for col in 0..width {
             let idx = row * width + col;
-            // Should hopefully make a checkerboard pattern
-            data[idx] = idx % 2 * row;
+            // Should hopefully make a gradient pattern
+            image.data[idx] = (idx % 2 * row) as u8;
         }
     }
 
     // Contents
-    for row in 0..height {
-        for col in 0..width {
-            let idx = row * width + col;
-            write!(output, "{} ", data[idx])?;
-        }
-    }
+    image.write(&mut output)?;
 
     Ok(())
 }
